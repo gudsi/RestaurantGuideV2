06c8f9fdbe2cf9a5d4ec1a823d977b63116b13d5
aydanassir <45256862+aydanassir@users.noreply.github.com>
2019-01-18 10:43:02 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/gudrun/test_guide/MainActivity.java
+++ app/src/main/java/com/example/gudrun/test_guide/MainActivity.java
@@ -42,5 +42,70 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    
+    
+    //the new code to replace the above one, i put it so may be u can compare it and see it both are not giving the required result anyways
+    TextView lat;
+    TextView lon;
+    String result="1000";
+    TextView showResponse;
+    Spinner spinner;
+    Button OSMButton;
+    JSONObject test = null;
+    int radius;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OSMButton = findViewById(R.id.button);
+        OSMButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callOSM();
+            }
+        });
+        spinner=(Spinner)findViewById(R.id.spinner);
+        String[] distancevalues= {"5000","10000","15000"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,distancevalues);
+        spinner.setAdapter(adapter);
+
+        int value = Integer.parseInt((String) spinner.getSelectedItem());
+        showResponse =findViewById(R.id.testText);
+    }
+
+    void callOSM()  {
+
+
+        final NetworkAsyncTask httpsTask = new NetworkAsyncTask();
+        httpsTask.execute();
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    final Object response = httpsTask.get();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showResponse.setText(Objects.toString(response));
+                            int value = Integer.parseInt((String) spinner.getSelectedItem());
+                            httpsTask.setvalue(value);
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+
+    }
+
 
 }
